a4af07c203b6f9056e7a095f65cf08b6bc82c43a
Christefan <chriscapod7@gmail.com>
2022-05-16 17:48:38 +0200

    Title+Jumbotron

---
+++ src/components/Hero.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="jumbotron">
+    <img src="../assets/img/logo.png" alt="" />
+    <h3>Questo e' una prova jumbotron</h3>
+  </div>
+</template>
+<script>
+export default {
+  name: "MyHero",
+};
+</script>
+<style scoped>
+h3 {
+  color: red;
+}
+.jumbotron {
+  border: 1px solid green;
+  width: 80%;
+  margin: 0 auto;
+}
+</style>
